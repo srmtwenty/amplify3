34d7b474605fb2e603102670b4d219fcee9f5413
srmtwenty <srmcho81.rd@gmail.com>
2023-11-20 13:17:02 -0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+} from "react-router-dom";
+//import About from './views/About';
+
+//import WorksAnim from './views/WorksAnim';
+//import WorksCode from './views/WorksCode';
+
+//import ResumeCode from './views/ResumeCode';
+//import ResumeAnim from './views/ResumeAnim';
+
+//import React from 'react';
+
+//import Collapsible from './views/Collapsible';
+
+
+//import Image from './images/concept1a.jpg';
+
+//import Resume from './views/Resume';
+//import Navbar from "./components/navbar";
 
 function App() {
   return (
---
+++ src/components/navbar/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from "./navbarElements";
+ 
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <Bars />
+ 
+                <NavMenu>
+                
+                    <NavLink to="/" >
+                        About
+                    </NavLink>
+                    <NavLink to="/works_anim" activeStyle>
+                        WorksAnim
+                    </NavLink>
+                    <NavLink to="/works_code" activeStyle>
+                        WorksCode
+                    </NavLink>
+                    <NavLink to="/resume_code" activeStyle>
+                        Resume Code
+                    </NavLink>
+                    
+                    <NavLink to="/resume" activeStyle>
+                        Resume
+                    </NavLink>
+                    {/* Second Nav */}
+                    {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+                </NavMenu>
+                
+            </Nav>
+        </>
+    );
+};
+ 
+export default Navbar;
