ea258f20c73ea39a6af0eab5c8a66955b2aeb5fd
srmtwenty <srmcho81.rd@gmail.com>
2024-03-12 22:31:50 -0700

    third commit in 2024

--- src/views/Page1.js
+++ src/views/Page1.js
@@ -10,9 +10,10 @@ function Page1(){
             <div style={{backgroundColor: "rgb(223, 243, 255)", marginLeft:"auto", marginRight:"auto", width: "70%"}}>
                 <div className="resumeStyle">
                     <h3 style={{textAlign: "center", paddingBottom: "10px"}}>*Choose a resume to view:</h3>
-                    <Link style={{fontSize:"16px", textDecoration: "none"}} class="current" to="/resume_code">Full Stack Developer</Link> 
-                    <Link class="currentOff" style={{fontSize:"16px", textDecoration: "none"}} to="/resume_anim">CG Animator</Link>
-                    
+                    <div>
+                        <Link style={{fontSize:"16px", textDecoration: "none"}} class="current" to="/resume_code">Full Stack Developer</Link> 
+                        <Link class="currentOff" style={{fontSize:"16px", textDecoration: "none"}} to="/resume_anim">CG Animator</Link>
+                    </div>
                     <SinglePagePDFViewer pdf={Scott_Cho_resume_full_stack_developer}/>
 
                     <div style={{textAlign:"center"}} >
--- src/views/Page2.js
+++ src/views/Page2.js
@@ -10,8 +10,11 @@ function Page2(){
             <div style={{backgroundColor: "rgb(223, 243, 255)", marginLeft:"auto", marginRight:"auto", width: "100%"}}>
                 <div className="resumeStyle">
                     <h3 style={{textAlign: "center", paddingBottom: "10px"}}>*Choose a resume to view:</h3>
-                    <Link className="currentOff" style={{fontSize:"16px", textDecoration: "none"}} to="/resume_code">Full Stack Developer</Link> 
-                    <Link style={{fontSize:"16px", textDecoration: "none"}} className="current" to="/resume_anim">CG Animator</Link>
+                    <div>
+                        <Link className="currentOff" style={{fontSize:"16px", textDecoration: "none"}} to="/resume_code">Full Stack Developer</Link> 
+                        <Link style={{fontSize:"16px", textDecoration: "none"}} className="current" to="/resume_anim">CG Animator</Link>
+                    
+                    </div>
                     
                     <SinglePagePDFViewer pdf={Scott_Cho_resume_cg_animator}/>
                     <div style={{textAlign:"center"}} >
