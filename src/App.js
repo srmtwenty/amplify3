418b0bbecc86c97913573a352939421249b4e836
srmtwenty <srmcho81.rd@gmail.com>
2024-03-14 19:06:38 -0700

    12th commit in 2024

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import WorksCode from './views/WorksCode';
 import Page1 from './views/Page1';
 import Page2 from './views/Page2';
 
-import React from 'react';
+import React, {useState} from 'react';
 
 //import Collapsible from './views/Collapsible';
 //import Image from './images/concept1a.jpg';
@@ -19,23 +19,39 @@ import React from 'react';
 //import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
 import Navbar from './components/navbar/Navbar';
-
+import {useSpring, animated} from 'react-spring';
 
 function App() {
+  const [flip, setFlip]=useState(false);
+  const props=useSpring({
+    to:{opacity:1},
+    from:{opacity:0},
+    //reset:true,
+    //reverse:flip,
+    delay:600,
+    onRest:()=>setFlip(!flip),
+  })
   return (
-    <Router>
-      <Navbar/>
+    <div className='App'>
+      {/*
+      <animated.div style={props}>*/}
+          <Router>
+            <Navbar/>
+          
+            <Routes>
+              <Route path="/" element={<About />}/>
+              <Route path="/works_anim" element={<WorksAnim/>}/>
+              <Route path="/works_code" element={<WorksCode/>}/>
+              <Route path="/resume_code" element={<Page1/>}/>
+              <Route path="/resume_anim" element={<Page2/>}/>
+            </Routes>
+            <Footer/>      
+          </Router>
+
+
+      {/*</animated.div>*/}
     
-      <Routes>
-        <Route path="/" element={<About />}/>
-        <Route path="/works_anim" element={<WorksAnim/>}/>
-        <Route path="/works_code" element={<WorksCode/>}/>
-        <Route path="/resume_code" element={<Page1/>}/>
-        <Route path="/resume_anim" element={<Page2/>}/>
-      </Routes>
-      <Footer/>      
-    </Router>
-    
+    </div>
   );
 }
 
---
+++ src/components/Fader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Fader=props=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+Fader.propTypes={
+
+}
+export default Fader;
